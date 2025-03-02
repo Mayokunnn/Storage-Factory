d9077025549ad38cd57b101974399760355ba896
Mayokunnn <kareola960@gmail.com>
2025-03-03 00:35:48 +0100

    starting over

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -1,33 +1,12 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.18;	
 
-// import {SimpleStorage, SimpleStorage2} from "./SimpleStorage.sol";
-import {SimpleStorage} from "./SimpleStorage.sol";
-
-contract StorageFactory {
-    SimpleStorage[] public listOfSimpleStorageContracts;
+contract StorageFactory{
 
     function createSimpleStorageContract() public {
-        SimpleStorage simpleStorageContractVariable = new SimpleStorage();
-        // SimpleStorage simpleStorage = new SimpleStorage();
-        listOfSimpleStorageContracts.push(simpleStorageContractVariable);
+        //How does the storage factory know what SimpleStorage looks like
+        
     }
 
-    function sfStore(
-        uint256 _simpleStorageIndex,
-        uint256 _simpleStorageNumber
-    ) public {
-        // Address
-        // ABI
-        // SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber);
-        listOfSimpleStorageContracts[_simpleStorageIndex].store(
-            _simpleStorageNumber
-        );
-    }
-
-    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
-        // return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
-        return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve();
-    }
 }
