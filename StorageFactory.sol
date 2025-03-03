b40a44f78fec1fa1be73a01d5b8aed1de542e7f0
Mayokunnn <kareola960@gmail.com>
2025-03-04 00:38:04 +0100

    import statements and named imports

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -1,11 +1,17 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.18;	
+pragma solidity ^0.8.18;
+import { SimpleStorage } from "./SimpleStorage.sol";
 
 contract StorageFactory{
 
+    //type visibility name
+    SimpleStorage public simpleStorage;
+    
     function createSimpleStorageContract() public {
         //How does the storage factory know what SimpleStorage looks like
+        simpleStorage = new SimpleStorage();
+          
         
     }
 
