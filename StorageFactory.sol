aef0c2c30ebc17cd79815c12f04664ce4206926d
Mayokunnn <kareola960@gmail.com>
2025-03-04 09:42:07 +0100

    interacting with contract's ABI and also inheritance

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -6,13 +6,24 @@ import { SimpleStorage } from "./SimpleStorage.sol";
 contract StorageFactory{
 
     //type visibility name
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public listOfSimpleStorageContracts;
+    address public deployedAddress;
     
     function createSimpleStorageContract() public {
         //How does the storage factory know what SimpleStorage looks like
-        simpleStorage = new SimpleStorage();
-          
+        SimpleStorage newSimpleStorageContract = new SimpleStorage();
+        deployedAddress = address(newSimpleStorageContract);
+        listOfSimpleStorageContracts.push(newSimpleStorageContract);
         
     }
 
+    function sfStore (uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+        //Address
+        //Application Binary Interface 
+        listOfSimpleStorageContracts[_simpleStorageIndex].store(_newSimpleStorageNumber);
+    }
+
+    function sfGet (uint256 _simpleStorageIndex) public view returns(uint256) {
+        return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve();
+    }
 }
